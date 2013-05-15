4b0ec2bb5549835eb31c9465a86798e254f73cf0
Alxe <alxegod@gmail.com>
2013-05-15 11:26:31 +0200

    Crear CargarMapa

---
+++ src/es/iessaladillo/juegos/saladillo/model/action/AccionCargarMapa.java
@@ -0,0 +1,11 @@
+package es.iessaladillo.juegos.saladillo.model.action;
+
+public class AccionCargarMapa implements Accion {
+
+	@Override
+	public Object ejecutar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
