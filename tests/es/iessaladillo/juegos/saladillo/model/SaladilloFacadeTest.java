a8ed7e9c920ff333f415a03b7cb22c96b21c7328
Alxe <alxegod@gmail.com>
2013-05-29 11:08:52 +0200

    Actualizados test JUnit

--- tests/es/iessaladillo/juegos/saladillo/model/SaladilloFacadeTest.java
+++ tests/es/iessaladillo/juegos/saladillo/model/SaladilloFacadeTest.java
@@ -6,16 +6,18 @@ import junit.framework.TestCase;
 import es.iessaladillo.juegos.saladillo.controller.Dibujable;
 import es.iessaladillo.juegos.saladillo.model.components.Mapa;
 import es.iessaladillo.juegos.saladillo.util.CargadorNiveles;
+import es.iessaladillo.juegos.saladillo.util.Direccion;
 import es.iessaladillo.juegos.saladillo.util.Entidad;
+import es.iessaladillo.juegos.saladillo.util.Posicion;
 
 public class SaladilloFacadeTest extends TestCase {
 	private static SaladilloFacade f = new SaladilloFacade();
-	private static Entidad[] entidades;
+	private static Mapa mapaPruebas;
 	
 	static {
 		ArrayList<Entidad> list = CargadorNiveles.cargarNivel("src/1.lvl");
 		
-		entidades = list.toArray(new Entidad[list.size()]);
+		mapaPruebas = (Mapa) f.mapaFromEntidades(list.toArray(new Entidad[list.size()]));
 	}
 	
 
@@ -24,7 +26,7 @@ public class SaladilloFacadeTest extends TestCase {
 	 *  y CargarMapa(MapaInterface)
 	 */
 	public void testCargarMapa() {
-		f.cargarMapa(entidades);
+		f.cargarMapa(mapaPruebas);
 		
 		Mapa mapa = (Mapa) f.getMapa();
 		
@@ -42,21 +44,30 @@ public class SaladilloFacadeTest extends TestCase {
 	 * clases que escapan del paquete Model
 	 */
 	public void testReiniciarNivel() {
-		f.cargarMapa(entidades);
+		f.cargarMapa(mapaPruebas);
 		
 		assertTrue(f.getMapaOriginal().equals(f.reiniciarNivel()));
 	}
 	
 	public void testDiamantesEnMapa() {
-		f.cargarMapa(entidades);
+		f.cargarMapa(mapaPruebas);
 		
 		assertTrue(f.diamantesEnMapa() == 4);
 	}
 	
-	public void testMapaToAscii() {
-		f.cargarMapa(entidades);
+	public void testPosicionHeroe() {
+		f.cargarMapa(mapaPruebas);
 		
-		String mapa = 
+		assertTrue(f.getPosicionHeroe().equals(new Posicion(1, 5)));
+	}
+	
+	/*
+	 * Engloba los casos de MapaToAscii y mover.
+	 */
+	public void testMapaToAscii() {
+		f.cargarMapa(mapaPruebas);
+		
+		assertTrue(f.mapaToAscii(f.getMapa()).equals(
 				"| · | · | · | · | · | · | · | · | · | · | · | · | · | · |\n" +
 				"| · | X | · | · | · | X | O | O | · | O | · | · | O | · |\n" +
 				"| X | · | X | · | · | X | $ | · | A | O | · | O | $ | O |\n" +
@@ -70,8 +81,25 @@ public class SaladilloFacadeTest extends TestCase {
 				"| O | · | O | · | · | · | · | · | O | $ | O | · | O | · |\n" +
 				"| · | O | · | O | · | · | · | O | O | O | O | O | · | · |\n" +
 				"| O | O | O | O | · | · | · | · | · | · | · | · | · | · |\n" +
-				"| $ | A | · | O | · | · | · | · | · | · | · | · | · | · |\n";
+				"| $ | A | · | O | · | · | · | · | · | · | · | · | · | · |\n"));
 		
-		assertTrue(f.mapaToAscii(f.getMapa()).equals(mapa));
+		
+		f.setMapa((Mapa) f.mover(Direccion.RIGHT));
+		
+		assertTrue(f.mapaToAscii(f.getMapa()).equals(
+				"| · | · | · | · | · | · | · | · | · | · | · | · | · | · |\n" +
+				"| · | X | · | · | · | X | O | O | · | O | · | · | O | · |\n" +
+				"| X | · | X | · | · | X | $ | · | A | O | · | O | $ | O |\n" +
+				"| · | A | · | · | · | X | O | O | O | O | · | · | O | · |\n" +
+				"| · | · | · | · | · | · | · | · | · | · | · | · | · | · |\n" +
+				"| · | · | @ | · | · | · | · | · | · | · | · | · | · | · |\n" +
+				"| · | · | · | · | · | · | · | · | · | · | · | · | · | · |\n" +
+				"| · | · | · | · | · | · | · | · | · | · | · | · | · | · |\n" +
+				"| O | · | O | · | · | · | · | · | O | · | O | · | · | · |\n" +
+				"| · | O | · | O | · | · | · | O | · | O | · | O | · | · |\n" +
+				"| O | · | O | · | · | · | · | · | O | $ | O | · | O | · |\n" +
+				"| · | O | · | O | · | · | · | O | O | O | O | O | · | · |\n" +
+				"| O | O | O | O | · | · | · | · | · | · | · | · | · | · |\n" +
+				"| $ | A | · | O | · | · | · | · | · | · | · | · | · | · |\n"));
 	}
 }
